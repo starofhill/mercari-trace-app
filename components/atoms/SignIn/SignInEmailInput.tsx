b959da63ecef4aea6997b499a21da2dff658d8e3
ryu0114h <ryu0114h@gmail.com>
2020-10-07 15:17:41 +0900

    SignUp機能の追加

--- components/atoms/SignIn/SignInEmailInput.tsx
+++ components/atoms/SignIn/SignInEmailInput.tsx
@@ -1,28 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
 import SafeAreaView from "react-native-safe-area-view";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
 
 const SignInEmailInput: React.FC = () => {
   const { navigate } = useNavigation();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = async () => {
+    console.log(email, password);
+
+    await axios
+      .post("https://mercari-trace-server.herokuapp.com/api/v1/auth/sign_in/", {
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        console.log(res);
+        console.log("サインイン完了");
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log("失敗");
+      });
+  };
 
   return (
     <SafeAreaView>
       <View style={styles.boxes}>
         <View style={styles.box}>
           <Text style={styles.textStyle}>メールアドレス</Text>
-          <TextInput placeholder="メールまたは電話番号" />
+          <TextInput
+            placeholder="メールまたは電話番号"
+            value={email}
+            onChangeText={(newValue) => {
+              setEmail(newValue);
+            }}
+          />
           <View />
         </View>
         <View style={styles.box}>
           <Text style={styles.textStyle}>パスワード</Text>
-          <TextInput placeholder="7文字以上の半角英数字" />
+          <TextInput
+            placeholder="7文字以上の半角英数字"
+            value={password}
+            onChangeText={(newValue) => {
+              setPassword(newValue);
+            }}
+          />
           <View />
         </View>
       </View>
       <View style={styles.buttonBox}>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={signIn}>
           <Text style={styles.buttonText}>ログイン</Text>
         </TouchableOpacity>
       </View>
